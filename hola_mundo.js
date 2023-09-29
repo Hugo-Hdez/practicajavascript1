6002d8a99f219281548c46d90856995a9854b65b
Hugo Esteban Hernández Beltrán <hernandez.beltran.hugo.esteban@gmail.com>
2023-09-29 10:48:48 -0600

    lectura con php

--- hola_mundo.js
+++ hola_mundo.js
@@ -1,1 +1,11 @@
-console.log("Hola mundo desde Node.js, esto se verá en la terminal de comandos")
+console.log("Hola mundo desde Node.js, esto se verá en la terminal de comandos, esto es backend 100%")
+
+console.log(2.5)
+
+console.log (window)
+
+console.log(global)
+
+setInterval(function(){
+    console.log('hola nodeJs')
+}, 1000)
