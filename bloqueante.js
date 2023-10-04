d31a794be03acb1a7d330d0eb3f7b63975e8a81f
Hugo Esteban Hernández Beltrán <hernandez.beltran.hugo.esteban@gmail.com>
2023-10-04 10:18:44 -0600

    Modelo bloqueante en nodejs

---
+++ bloqueante.js
@@ -0,0 +1,9 @@
+const { captureRejectionSymbol } = require("events");
+var fs = require("fs");
+console.log("\nAbriendo Archivo . . .");
+
+var content = fs.readFileSync("archivo.txt");
+console.log(content);
+
+console.log("\nHaciendo otra cosa");
+console.log(process.uptime());
